3097c27da0059cddde44b51593d767e7044c5be4
complainbadi <complainbadi@gmail.com>
2025-10-17 19:04:59 +0000

    ad

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,10 +1,19 @@
-
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Get the origin of the development server
+const origin = new URL(process.env.EXPO_DEV_SERVER_ORIGIN || 'http://localhost:8081');
+
 config.server = {
   ...config.server,
+  // Remap the origin of the development server to allow requests from the cloud environment
+  remapCDN: [
+    {
+      origin: origin.origin,
+      cdn: process.env.EXPO_PUBLIC_DEV_URL,
+    },
+  ],
   rewriteRequestUrl: (url) => {
     if (!url.endsWith('.bundle')) {
       return url;
